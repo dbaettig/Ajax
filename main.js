43fa33fd5860871d64b516670e985ab22c32df3d
dbaettig <daniela.baettig@hotmail.com>
2018-03-15 12:11:48 +0100

    added clickable div function for more info on movie

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const infoElement = document.getElementById('infoElement');
 const input = document.getElementById('input');
 const category = document.querySelector('#category');
 const searchButton = document.getElementById('searchButton');
+
+
+
 let allFilms = [];
+let allFilmDesc = [];
+let id = 12101;
 
 
 fetchFilms();
@@ -18,6 +23,32 @@ function fetchFilms() {
         .then(function (films) {
             displayFilms(films);
             allFilms = films;
+            //console.log(films);
+
+
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+
+fetchFilmDesc();
+
+function fetchFilmDesc() {
+
+    fetch(`https://cors-anywhere.herokuapp.com/http://api.stockholmfilmfestival.se/v1/films/film/film_id/${id}/festival_id/29/format/json/API-Key/70kbAllS0oDsaI9ho5oaRL3Ey7CdqSdkRB0Pmihk`)
+
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (filmDesc) {
+            displayFilmDesc(filmDesc);
+            allFilmDesc = filmDesc;
+            //console.log(filmDesc);
+
 
 
         })
@@ -31,39 +62,73 @@ function fetchFilms() {
 
 
 // ------- SHOWS ALL MOVIES --------//
+//
+//function displayFilmDesc(filmDesc) {
+//let id = filmDesc.filmId;
+//    let info = '';
+//   for (var i = 0; i < filmDesc.length; i++) {
+//    
+//    
+//    info +=
+//        console.log(filmDesc.filmId);
+//        ` <div class ="films">
+//            <h3> ${filmDesc.filmName} </h3>
+//            <p> ${filmDesc.filmDirector} </p>
+//            <p> ${filmDesc.filmId} </p>
+//            <p> ${filmDesc.sectionName} </p>  
+//        </div>
+//    `
+//
+//    ;
+//
+//    infoElement.innerHTML = info;
+//
+//
+//}
+//
+//    }
+
 
 function displayFilms(films) {
 
     let info = '';
     for (var i = 0; i < films.length; i++) {
-        
-        // const IMAGESIZE = (320 x 180);
-        info += `
-        <div class="films">
-            
+        let id = films[i].filmId;
+
+        // console.log(films[i].filmId)
+
+
+        info +=
+
+            ` <div class ="films">
             <h3> ${films[i].filmName} </h3>
-            <p> ${films[i].filmDirector} </p> 
-            <p> ${films[i].sectionName} </p>
-
+            <p> ${films[i].filmDirector} </p>
+            <p> ${films[i].filmId} </p>
+            <p> ${films[i].sectionName} </p>  
         </div>
-        `;
+    `
+
+        ;
+
         infoElement.innerHTML = info;
- }
-    
-        }
 
 
+    }
 
- searchButton.addEventListener('click', function(){
-     let info = '';
-     const inputValue = input.value;
-    
-     for (var i = 0; i < allFilms.length; i++) {    
-       
-      
-         if(inputValue == allFilms[i].filmId){
-          // const IMAGESIZE = (320 x 180);
-        info += `
+
+}
+
+// ----- SEARCH INPUT FOR MOVIE TITLE ---- //
+
+searchButton.addEventListener('click', function () {
+    let info = '';
+    const inputValue = input.value;
+    for (var i = 0; i < allFilms.length; i++) {
+
+        if (inputValue.toUpperCase().split(' ').join('') ==
+            allFilms[i].filmName.toUpperCase().split(' ').join('')) {
+
+            info += `
         <div class="films">
             
             <h3> ${allFilms[i].filmName} </h3>
@@ -72,18 +137,16 @@ function displayFilms(films) {
 
         </div>
         `;
-        infoElement.innerHTML = info;
-         }
-     }
-                               
-});
- 
-    
+            infoElement.innerHTML = info;
+        }
+    }
 
-   
+});
+
 
 
 //------ EVENTLISTNER FOR ALL CATEGORY BUTTONS -----//
+
 category.addEventListener('click', getSection);
 
 
@@ -133,10 +196,37 @@ function getSection(section) {
             <h3> ${mySections[i].filmName} </h3>
             <p> ${mySections[i].filmDirector} </p> 
             <p> ${mySections[i].sectionName} </p>
+          
 
         </div>
         `;
 
         infoElement.innerHTML = info;
+    }
+}
+
+
+
+infoElement.addEventListener('click', displayFilmDesc);
+
+function displayFilmDesc(filmDesc) {
+    
+    if (filmDesc.target !== filmDesc.currentTarget) {
+        var clickedDiv = filmDesc.target.innerHTML;
+
+        console.log(clickedDiv);
+
+        let info = " ";
+
+        info +=
+            `  <div class="films">
+
+            ${clickedDiv}
+            <p> ${allFilmDesc.filmDescription_en} </p> 
+
+ </div>
+ `;
+        infoElement.innerHTML = info;
+
     }
 }
